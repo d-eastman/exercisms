cdac7ce653914e93ca29f43311c16ca69fdf7074
DE <fuzzylikeagangster@yahoo.com>
2014-10-30 12:40:57 -0400

    Add Hamming projects

---
+++ Hamming.Library/Hamming.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamming.Library
+{
+    public static class Hamming
+    {
+        /// <summary>
+        /// Compute the Hamming distance between two DNA strands.  Each point mutation adds one to
+        /// the calculated distance.
+        /// </summary>
+        /// <param name="strand1">Nucleotide DNA sequence 1 as string of the characters A, C, G, and T.</param>
+        /// <param name="strand2">Nucleotide DNA sequence 2</param>
+        /// <returns>Hamming distance, the number of point differences between the two DNA strands.</returns>
+        public static int Compute(string strand1, string strand2)
+        {
+            if (strand1.Length != strand2.Length)
+                throw new ArgumentException("Strands must be equal length.");
+
+            int distance = 0;
+
+            //Compare all points and increment distance if they are different.
+            for (int i = 0; i < strand1.Length; i++)
+            {
+                if (strand1[i] != strand2[i])
+                    distance++;
+            }
+
+            return distance;
+        }
+    }
+}
---
+++ Hamming.Test/HammingTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using HL = Hamming.Library;
+
+[TestFixture]
+public class HammingTest
+{
+    [Test]
+    public void No_difference_between_empty_strands()
+    {
+        Assert.That(HL.Hamming.Compute("", ""), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void No_difference_between_identical_strands()
+    {
+        Assert.That(HL.Hamming.Compute("GGACTGA", "GGACTGA"), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Complete_hamming_distance_in_small_strand()
+    {
+        Assert.That(HL.Hamming.Compute("ACT", "GGA"), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Hamming_distance_is_off_by_one_strand()
+    {
+        Assert.That(HL.Hamming.Compute("GGACGGATTCTG", "AGGACGGATTCT"), Is.EqualTo(9));
+    }
+
+    [Test]
+    public void Smalling_hamming_distance_in_middle_somewhere()
+    {
+        Assert.That(HL.Hamming.Compute("GGACG", "GGTCG"), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Larger_distance()
+    {
+        Assert.That(HL.Hamming.Compute("ACCAGGG", "ACTATGG"), Is.EqualTo(2));
+    }
+
+    [Test]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Unequal_sized_strands()
+    {
+        HL.Hamming.Compute("AA", "A");
+    }
+}
