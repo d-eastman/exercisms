5d5d422b60a67ef32ba876901c7a8441a35446c5
fuzzylikeagangster <fuzzylikeagangster@yahoo.com>
2014-11-04 10:27:33 -0500

    Simplified the code comparing if two strings are anagrams.  Did away with more complicated dictionary of chars and char counts and replaced with simple sorted string.

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -35,14 +35,15 @@ namespace Anagram.Library
         {
             List<string> retList = new List<string>(); //Initialize list to hold return values
 
-            var baseComposition = GetWordComposition(BaseWord); //Get our word's composition
+            string sortedBaseWord = sortCharsInString(BaseWord);
 
             foreach (string tw in testWords)
             {
-                if (!String.IsNullOrWhiteSpace(tw) && tw.ToLower() != BaseWord.ToLower() && tw.Length == BaseWord.Length)
+                if (!String.IsNullOrWhiteSpace(tw) && tw.ToLower() != BaseWord.ToLower()) // && tw.Length == BaseWord.Length)
                 {
                     //If test word is not the base word and the two words are the same length, then compare the compositions
-                    if (CharIntDictionariesAreIdentical(baseComposition, GetWordComposition(tw)))
+                    //if (CharIntDictionariesAreIdentical(baseComposition, GetWordComposition(tw)))
+                    if (sortCharsInString(tw).Equals(sortedBaseWord))
                     {
                         retList.Add(tw); //Words are anagrams, so add to return list of words
                     }
@@ -53,49 +54,15 @@ namespace Anagram.Library
         }
 
         /// <summary>
-        /// Break a word down into a dictionary of sorted unique characters (dictionary keys) and character counts (dictionary values).  
-        /// Note: converting ToDictionary does not automatically sort the dictionary entries by key, so that's done in LINQ.
+        /// Sort the chars in a string alphabetically in a case-insensitive way and return those chars as a new string
         /// </summary>
-        /// <param name="word">Word to decompose</param>
-        /// <returns>Dictionary of unique characters in the word and how many times each appears in the word.  Sorted by key.</returns>
-        protected Dictionary<char, int> GetWordComposition(string word)
+        /// <param name="text">String to be sorted</param>
+        /// <returns>Sorted string of characters</returns>
+        protected string sortCharsInString(string text)
         {
-            return (from c in word.ToLower().ToCharArray()
-                    group c by c into g
-                    orderby g.Key
-                    select new KeyValuePair<char, int>(g.Key, g.Count())
-                    ).ToDictionary(x => x.Key, y => y.Value);
-        }
-
-        /// <summary>
-        /// Compare all keys and values of two dictionary objects and return true if everything is exactly
-        /// the same, otherwise return false if anything does not match.  Note: returns false if either object is null.
-        /// </summary>
-        /// <param name="d1">First char-int dictionary object</param>
-        /// <param name="d2">Second char-int dictionary object</param>
-        /// <returns>True means the dictionary objects have identical keys and values.</returns>
-        protected bool CharIntDictionariesAreIdentical(Dictionary<char, int> d1, Dictionary<char, int> d2)
-        {
-            if (d1 == null || d2 == null)
-                return false; //One or both objects are null
-
-            if (d1.Keys.Count != d2.Keys.Count)
-                return false; //Number of unique characters is different
-
-            //Get an enumerator for each dictionary
-            var e1 = d1.GetEnumerator();
-            var e2 = d2.GetEnumerator();
-
-            //Loop through both enumerators in lockstep to compare the key and value of the dictionary entries.
-            while (e1.MoveNext())
-            {
-                e2.MoveNext(); //Move to next element on 2nd dictionary to stay in step with first enumerator
-                if (!e1.Current.Key.Equals(e2.Current.Key) || !e1.Current.Value.Equals(e2.Current.Value))
-                    return false; //A key or value did not match
-            }
-
-            //If execution gets to here, then all keys and all values match
-            return true;
+            char[] chars = text.ToLower().ToCharArray();
+            Array.Sort(chars);
+            return new string(chars);
         }
     }
 }
