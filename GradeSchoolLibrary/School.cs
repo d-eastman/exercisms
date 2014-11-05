cf43261eddaf298ac22f27b21289074cf6b1782f
fuzzylikeagangster <fuzzylikeagangster@yahoo.com>
2014-11-05 09:09:03 -0500

    Add Grade School library and test project

---
+++ GradeSchoolLibrary/School.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeSchoolLibrary
+{
+    public class School
+    {
+        public Dictionary<int, List<string>> Roster { get; private set; }
+
+        public School()
+        {
+            Roster = new Dictionary<int, List<string>>();
+        }
+
+        public void Add(string name, int grade)
+        {
+            ensureSchoolHasGrade(grade);
+            Roster[grade].Add(name);
+            Roster[grade].Sort();
+        }
+
+        public List<string> Grade(int grade)
+        {
+            ensureSchoolHasGrade(grade);
+            return Roster[grade];
+        }
+
+        private void ensureSchoolHasGrade(int grade)
+        {
+            if (!Roster.ContainsKey(grade))
+            {
+                Roster.Add(grade, new List<string>());
+            }
+        }
+    }
+}
